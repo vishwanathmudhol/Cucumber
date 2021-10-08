3805392c921bb66a8f6155611c09c1206ebb83b6
Vishwanath <vishwanath.mudhol@gmail.com>
2021-10-08 13:28:37 +0530

    Commit to check PR

--- src/test/java/StepDefinitions/SearchFlight.java
+++ src/test/java/StepDefinitions/SearchFlight.java
@@ -20,7 +20,7 @@ public class SearchFlight {
       driver = new ChromeDriver();
 	   driver.manage().window().maximize();
 	   driver.get("https://www.cleartrip.com/");
-//	   WebDriver dr = new FirefoxDriver();
+
 	  
 	}
 
